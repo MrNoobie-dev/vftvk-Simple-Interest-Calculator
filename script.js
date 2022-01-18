4288bd7a4f8aeb5790038231d077009b943afadc
MrNoobie-dev <bdedi07@gmail.com>
2022-01-18 22:11:29 +0200

    Project Completed

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+window.addEventListener('load', function () {
+    let rate = document.forms["form"]['rate'];
+    let output = document.getElementById('output');
+    output.innerHTML = rate.value+"%";
+    rate.oninput = function() {
+        output.innerHTML = this.value+"%";
+      };
+  });
+
+  function compute(e)
+  {
+    e.preventDefault();
+    let principal = document.forms["form"]['principal'].value;
+    let interestRate = document.forms["form"]['rate'].value;
+    let years = document.forms["form"]['year'].value;
+    let result = (principal * (interestRate/100)) * years;
+    console.log(interestRate/100)
+    document.getElementById('result').innerHTML = 
+    `If you deposit ${principal},<br>at an interest rate of ${interestRate}%.<br>You will receive an amount of ${result.toFixed(0)},<br>in the year ${2022+parseInt(years)}`;
+  };
+
